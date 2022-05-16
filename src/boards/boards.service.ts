ea802893221e816cd4bd9cf1429f1c138af96adb
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-16 23:19:44 +0900

    FEAT: Delete board by id 예외처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -20,7 +20,8 @@ export class BoardsService {
   }
 
   deleteBoardById(id: string): void {
-    this.boards = this.boards.filter((board) => board.id !== id);
+    const board = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== board.id);
   }
 
   updateBoardStatusById(id: string, status: BoardStatus): Board {
