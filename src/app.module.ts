0c24d1b754ae3c7a50cc14fd74ff2bde01b31bd1
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-16 22:11:16 +0900

    FEAT: Get all boards, Create one board

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [BoardsModule],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
