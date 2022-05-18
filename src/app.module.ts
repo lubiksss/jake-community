7257e967e7adcc73713cc8f32472f51cb05f1525
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-18 23:55:55 +0900

    FEAT: type orm 사용, entity, repository 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
   controllers: [AppController],
   providers: [],
 })
