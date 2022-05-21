94ce5d7c75deca495d842ea32b27e8dfe98ba189
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-21 15:37:38 +0900

    FEAT: auth module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { BoardsModule } from './boards/boards.module';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
