2b162a2d2f12cdfa867741e4f8898ed49d9d5960
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-21 16:33:36 +0900

    FEAT: user 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  @UsePipes(ValidationPipe)
+  singUp(@Body() authCredentialDto: AuthCredentialDto): Promise<void> {
+    return this.authService.signUp(authCredentialDto);
+  }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,23 @@
+import { User } from './entity/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Existing username');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+}
