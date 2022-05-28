5fdff52ae9593fbbe2e2ea56af23a59124cd1a79
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-28 12:45:03 +0900

    FEAT: 카카오 passport사용하여 Auth구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { KakaoAuthModule } from './kakao-auth/kakao-auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule, KakaoAuthModule],
   controllers: [AppController],
   providers: [],
 })
