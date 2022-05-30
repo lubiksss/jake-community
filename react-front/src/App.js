02d7d9502fbc2f713fc81d73ece93f8ab91ac4b9
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-30 22:41:02 +0900

    FEAT: react로 front 연결

---
+++ react-front/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+function App() {
+  return <div>Hello React & NestJS</div>;
+}
+
+export default App;
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule,
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'react-front/build/'),
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
